c2b6838c22b5d7d3dfadb29c6cef614ccc40361d
Norris Envoy <nenvoy@ga-s5.msk.21-school.ru>
2022-04-21 17:26:35 +0300

    day 3

--- libft/libft.h
+++ libft/libft.h
@@ -33,6 +33,8 @@ int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strcmp(const char *s1, const char *s2);
+char	*ft_strcpy(char *dst, const char *src);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
--- main.c
+++ main.c
@@ -1,14 +1,27 @@
 #include "minishell.h"
+#include <stdio.h>
+#include <string.h>
 
 int	g_status;
+
+static void	test(t_env	*envm, char *cmd)
+{
+	if (ft_strcmp("env", cmd))
+		m_env(envm);
+	if (ft_strcmp("pwd", cmd))
+		m_pwd();
+//	if (ft_strcmp("echo", cmd))
+//		m_echo(envm);
+}
+
 
 int	main(int argc, char **argv, char **envp)
 {
 	t_env	*envm;
 	char	*cmd;
 
-	(void)argv;
 	(void)argc;
+	(void)argv;
 	g_status = 0;
 	envm = init_env();
 	envm->cp_path = ft_split(getenv("PATH"), ':');
@@ -19,6 +32,7 @@ int	main(int argc, char **argv, char **envp)
 	{
 		cmd = readline(BEGIN(49, 31)"hellshell$> "CLOSE);
 		add_history(cmd);
-		free(cmd);
+		test(envm, cmd);
+		printf("%s", cmd + 5);
 	}
 }
